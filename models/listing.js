d7ae086bf8acfd66dd50da3ff892c9d157dd5386
SUMITA123-maker <sumitamishra20201@gmail.com>
2025-09-04 14:55:14 +0530

    Added default image

--- models/listing.js
+++ models/listing.js
@@ -11,11 +11,20 @@ const listingSchema = new Schema({
     description: {
         type: String,
     },
-    image: [{
-     filename: String,
-     url: String
-    }
-      ],
+    image: {
+    type: [
+      {
+        filename: String,
+        url: String
+      }
+    ],
+    default: [
+      {
+        filename: "default.jpg",
+        url: "https://images.unsplash.com/photo-1586611292717-f828b167408c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }
+    ]
+  },
     price:{
         type: Number,
         require: true,
